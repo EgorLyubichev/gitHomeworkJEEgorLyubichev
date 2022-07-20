172d6adbd8e4db8befa7de196cad519ec6b29dcf
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-07-20 17:51:09 +0300

    Created third line in fourth branch

--- src/main/java/by/lev/fourthBranch/Main.java
+++ src/main/java/by/lev/fourthBranch/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from fourth branch!");
         System.out.println("This line is for cherry pick");
+        System.out.println("Third line in fourth branch");
     }
 }
