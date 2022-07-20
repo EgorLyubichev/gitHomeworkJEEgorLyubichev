fb196c5a3a5258fbc59b478d7d6d6b816c98edaf
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-07-20 17:36:54 +0300

    Added third line in master

--- src/main/java/by/lev/master/Main.java
+++ src/main/java/by/lev/master/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from master!");
         System.out.println("Added second line in master");
+        System.out.println("Added third line in master");
     }
 }
