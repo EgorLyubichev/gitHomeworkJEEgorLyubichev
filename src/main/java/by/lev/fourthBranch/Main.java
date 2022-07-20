47f74d845233f6edb74e4b099623266a8d2dcf2b
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-07-20 17:45:18 +0300

    Added line with cherry pick

---
+++ src/main/java/by/lev/fourthBranch/Main.java
@@ -0,0 +1,8 @@
+package by.lev.fourthBranch;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from fourth branch!");
+        System.out.println("This line is for cherry pick");
+    }
+}
