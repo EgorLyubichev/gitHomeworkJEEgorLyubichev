ec27a778734a0fe14fd2732c86dd3d50f68d6b41
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-07-20 17:25:14 +0300

    Added second line in master

--- src/main/java/by/lev/master/Main.java
+++ src/main/java/by/lev/master/Main.java
@@ -3,5 +3,6 @@ package by.lev.master;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from master!");
+        System.out.println("Added second line in master");
     }
 }
