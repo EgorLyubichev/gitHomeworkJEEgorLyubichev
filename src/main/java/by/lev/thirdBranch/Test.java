c0ecec642ed4306d195bba2344022b4f2a0d6dfa
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-07-20 17:35:43 +0300

    Added second line in third branch

--- src/main/java/by/lev/thirdBranch/Test.java
+++ src/main/java/by/lev/thirdBranch/Test.java
@@ -3,5 +3,6 @@ package by.lev.thirdBranch;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello from third branch!");
+        System.out.println("Added second line in third branch");
     }
 }
