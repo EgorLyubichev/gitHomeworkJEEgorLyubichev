d802fd8344021151b15122ca433bdeb3c51d4dba
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-07-20 17:47:26 +0300

    Something new in master

--- src/main/java/by/lev/master/Main.java
+++ src/main/java/by/lev/master/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello from master!");
         System.out.println("Added second line in master");
         System.out.println("Added third line in master");
+        System.out.println("I decided to write something new");
     }
 }
