59673bb51a9a53f7a1b48009243a6511ad135691
Egor Lyubichev <EgorLyubichev@mail.ru>
2022-07-20 17:45:18 +0300

    Created line for cherry pick action

--- src/main/java/by/lev/fourthBranch/Main.java
+++ src/main/java/by/lev/fourthBranch/Main.java
@@ -3,5 +3,6 @@ package by.lev.fourthBranch;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from fourth branch!");
+        System.out.println("This line is for cherry pick");
     }
 }
